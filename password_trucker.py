e08c3b0a45f812801edd5201cafe7c25b4b211b2
richardkefa <richardkefa@gmail.com>
2020-09-06 15:02:55 +0300

    add login exixt test

--- password_trucker.py
+++ password_trucker.py
@@ -1,6 +1,6 @@
 class User:
   '''
-  User credential class 
+  User login info class 
   '''
   def __init__(self,username,password):
 
@@ -9,4 +9,62 @@ class User:
 
   user_list=[]
   
+  def save_user(self):
+    '''
+    saves user object to user list
+    '''
+    User.user_list.append(self)
+  
+  @classmethod
+  def user_exists(cls,usernames,passwords):
+    '''
+    metod takes login credentials and compares them to saved ones before allowing user to login
+    '''
+    for user in cls.user_list:
+      if user.username == usernames and user.password == passwords:
+         return True
+    return False  
 
+class Credentials:
+  '''
+  User cresentils class
+  '''
+  def __init__(self,account,account_username,account_password):
+    
+    self.account = account
+    self.account_password = account_password
+    self.account_username = account_username
+    
+  credentials_list = []
+  
+  def save_credentials(self):
+    '''
+    saves credential object to credential list
+    '''
+    Credentials.credentials_list.append(self)
+  @classmethod
+  def credential_search(cls,account):
+    '''
+    Searching Through credentials by accouunt
+    
+    Args:
+    account: account to ser for
+    returns:
+    credentials that match the account
+    '''
+    for credentials in cls.credentials_list:
+      if credentials.account == account:
+        return credentials
+  @classmethod
+  def delete_credential(self):
+    '''
+    delete_credential method deletes saved credential from the credentials_list
+    '''
+    Credentials.credentials_list.remove(self)
+    
+  @classmethod
+  def display_credentials(cls):
+    return cls.credentials_list
+
+    
+    
--- test_password_tracker.py
+++ test_password_tracker.py
@@ -1,26 +1,57 @@
 import unittest
-from password_trucker import User #importing user class 
+from password_trucker import User  # importing user class
+from password_trucker import Credentials # importing credentials class
+
 
 class TestUser (unittest.TestCase):
   '''
   Defined test case for user class
   '''
+
+  def setUp(self):
+    '''
+    fixture setup
+    '''
+    self.new_user = User("richard", "#12345")
+
   def test_init(self):
     '''
-    testing object initialization
+    testing objest initialization
     '''
-    def setUp(self):
-      '''
-      fixture setup
-      '''
-      self.new_user = User("richardkefa","#12345")
-    def test_init(self):
-      '''
-      testing objest initialization
-      '''
-      self.assertEqual(self.new_user.username,"richard")
-      self.assertEqual(self.new_user.password,"#12345")
+    self.assertEqual(self.new_user.username, "richard")
+    self.assertEqual(self.new_user.password, "#12345")
+
+  def test_save_user(self):
+    '''
+    testing saving user credentials
+     '''
+    self.new_user.save_user()  # saving new user
+    self.assertEqual(len(User.user_list),1)
+    
+  def tearDown(self):
+    '''
+    method to clean up after every test
+    '''
+    User.user_list=[]
+    
+  # def test_login(self):
+  #   '''
+  #   testing validity of user credentials
+  #   '''
+  #   self.new_user.save_user()
+  #   test_user = User("kefa","12345")
+  #   test_user.save_user()
+  #   login_info = User.user_login("kefa","12345")
+  #   self.assertTrue(login_info)
+    
+  def test_login_exists(self):
+    self.new_user.save_user()
+    test_user = User("kefa","12345")
+    test_user.save_user()
+    user_exists = User.user_exists("richard","#12345")
+    self.assertTrue(user_exists)
+
+
 
 if __name__ == "__main__":
     unittest.main()
-
